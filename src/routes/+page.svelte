dbb460079cc82094012ba7418182377aebd30796
Garen <garengh4@gmail.com>
2022-11-04 13:29:53 -0400

    homepage layout complete?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,89 @@
-hello from fallback +page.svelte
-<div class="text-column">
-  hello from text-column
+<div class="container">
+	<nav>NavBar</nav>
+	<main>About me: resume</main>
+	<div id="sidebar">Sidebar: Github Linkedin</div>
+	<div id="content-1">Project 1</div>
+	<div id="content-2">Project 2</div>
+	<div id="content-3">Project 3</div>
+	<footer>Footer</footer>
 </div>
-<div class="grid-container ">
-	<div class="left-console">LEFT</div>
-  <h1>hello h1</h1>
-  <a href="#">hello a</a>
-  <p>hello p</p>
-</div>
-TODO: FIX
 
 <style>
-	.grid-container {
+  :root{
+    --main-radius:5px;
+    --main-padding: 10px;
+  }
+	.container {
 		display: grid;
-		width: 100%;
-		height: 250px;
+		height: 100vh;
+		grid-template-columns: 1fr 1fr 1fr 1fr;
+		grid-template-rows: 0.2fr 1.5fr 1.2fr 0.8fr;
 		grid-template-areas:
-			'head head'
-			'nav  main'
-			'nav  foot';
-		grid-template-rows: 50px 1fr 30px;
-		grid-template-columns: 150px 1fr;
+			'nav nav nav nav'
+			'sidebar main main main'
+			'sidebar content-1 content-2 content-3'
+			'sidebar footer footer footer';
+		gap: 0.4rem;
+    font-family: 'Times New Roman', Times, serif;
+    font-weight: 800;
+    font-size: 20px;
+    color: #EAEFB4;
+
 	}
-	.grid-container > div {
+	nav {
+		background: #a7ffeb;
 		grid-area: nav;
-		background-color: #ffa08c;
+    border-radius: var(--main-radius);
+    padding-top: var(--main-padding);
+	}
+	main {
+		background: #1D2C4B;
+		grid-area: main;
+    border-radius: var(--main-radius);
+    padding-top: var(--main-padding);
+	}
+	#sidebar {
+		background: #393F48;
+		grid-area: sidebar;
+    border-radius: var(--main-radius);
+    padding-top: var(--main-padding);
+	}
+	#content-1 {
+		background: lightcoral;
+		grid-area: content-1;
+    border-radius: var(--main-radius);
+    padding-top: var(--main-padding);
+	}
+	#content-2 {
+		background: lightgray;
+		grid-area: content-2;
+    border-radius: var(--main-radius);
+    padding-top: var(--main-padding);
+	}
+	#content-3 {
+		background: lemonchiffon;
+		grid-area: content-3;
+    border-radius: var(--main-radius);
+    padding-top: var(--main-padding);
+	}
+	footer {
+		background: peru;
+		grid-area: footer;
+    border-radius: var(--main-radius);
+    padding-top: var(--main-padding);
+	}
+	@media only screen and (max-width: 550px) {
+		.container {
+			grid-template-columns: 1fr;
+			grid-template-rows: 0.4fr 0.4fr 2.2fr 1.2fr 1.2f 1.2fr 1fr;
+			grid-template-areas:
+				'nav'
+				'sidebar'
+				'main'
+				'content-1'
+				'content-2'
+				'content-3'
+				'footer';
+		}
 	}
 </style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -18,7 +18,7 @@
 				<a href="/">Home</a>
 			</li>
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+				<a href="/about">Blogs</a>
 			</li>
 			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
 				<a href="/sverdle">Sverdle</a>
